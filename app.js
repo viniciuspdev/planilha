f4d13d1c0da8d2f143d733350c3a4e8197dec6a8
Vinícius Pacheco <viniciusmpacheco22@gmail.com>
2022-08-23 18:54:50 +0000

    criando servidor

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hello world!");
+const http = require('http');
+
+const port = 3001;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Ola mundo!!!!');
+});
+
+server.listen(port, () => {
+    console.log(`Servidor ouvindo na porta ${port}`);
+});
---
+++ fizzBuzz.js
@@ -0,0 +1,23 @@
+function fizzBuzz(numero) {
+    let mensagem = "";
+    if (numero % 3 == 0) {
+        mensagem += 'Fizz';
+    }
+
+    if (numero % 5 == 0) {
+        mensagem += 'Buzz';
+    }
+
+    if (mensagem.length == 0) {
+        mensagem += String(numero);
+    }
+
+    return mensagem;
+}
+
+for (let i = 0; i <= 20; i++) {
+    const mensagem = fizzBuzz(i);
+    console.log(mensagem);
+}
+
+module.exports.fizzBuzz = fizzBuzz;
