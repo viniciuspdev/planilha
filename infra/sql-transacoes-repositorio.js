a0f714122bd618aa4108111faa2869ba0fc99001
Vinícius Pacheco <viniciusmpacheco22@gmail.com>
2022-08-31 19:25:30 +0000

    corrigindo post

--- infra/sql-transacoes-repositorio.js
+++ infra/sql-transacoes-repositorio.js
@@ -1,19 +1,51 @@
-const { Pool } = require('pg')
-const pool = new Pool()
-class SqlTransacoesRepositorio {
+const { Pool } = require("pg");
+const pool = new Pool();
 
+class SqlTransacoesRepositorio {
     async listarTransacoes() {
-        const resultado = await pool.query('SELECT * FROM transacoes')
-        console.log(resultado.rows)
+        const resultado = await pool.query("SELECT * FROM transacoes");
+        console.log(resultado.rows);
         return {
-            transacoes: resultado.rows
-        }
+            transacoes: resultado.rows,
+        };
     }
 
     async criarTransacao(transacao) {
-        const lista = transacoes.transacoes
-        lista.push(transacao)
+        const consulta = `INSERT INTO
+          transacoes(valor, descricao, categoria)
+          VALUES ($1, $2, $3) RETURNING *`;const { Pool } = require("pg");
+const pool = new Pool();
+
+class SqlTransacoesRepositorio {
+  async listarTransacoes() {
+    const resultado = await pool.query("SELECT * FROM transacoes");
+    console.log(resultado.rows);
+    return {
+      transacoes: resultado.rows,
+    };
+  }
+
+  async criarTransacao(transacao) {
+    const consulta = `INSERT INTO
+          transacoes(valor, descricao, categoria)
+          VALUES ($1, $2, $3) RETURNING *`;
+    const valores = [
+        transacao.valor,
+        transacao.descricao,
+        transacao.categoria
+    ];
+    await pool.query(consulta, valores)
+  }
+}
+
+module.exports = SqlTransacoesRepositorio;
+        const valores = [
+            transacao.valor,
+            transacao.descricao,
+            transacao.categoria
+        ];
+        await pool.query(consulta, valores)
     }
 }
 
-module.exports = SqlTransacoesRepositorio
+module.exports = SqlTransacoesRepositorio;
